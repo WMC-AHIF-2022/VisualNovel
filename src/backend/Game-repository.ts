f3ff5e0f6bcc13de4034aac00c3ea9461ea5ce63
LisaPich <pichlerlisa434@gmail.com>
2023-04-24 16:46:28 +0200

    Scene and Game repository

--- src/backend/Game-repository.ts
+++ src/backend/Game-repository.ts
@@ -1,4 +1,4 @@
-import {IScene} from "./Scene-repository";
+import {IScene, IScenePictures} from "./Scene-repository";
 
 export interface IGame {
     id: number,
@@ -13,4 +13,45 @@ export interface IGameinfo{
     releaseDate: Date
 }
 
-let IGame
+let nextId = 0;
+
+let games:IGame[] = [];
+
+function findGameIndex(id: number) {
+    for (let i = 0; i < games.length; i++) {
+        if (games[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+export function getAllGames() {
+    return games;
+}
+
+export function getGameById(id: number): IGame|undefined {
+    const index: number = findGameIndex(id);
+    if (index < 0) {
+        return undefined;
+    }
+    return games[index];
+}
+
+export function addGame(nextId: number, scenes: IScene[],infos: IGameinfo): IGame {
+    const game: IGame = { id: nextId++, scenes: scenes, infos: infos};
+    games.push(game);
+    return game;
+}
+
+export function removeAllGames(): void {
+    games.splice(0);
+}
+
+export function removeGame(game: IGame): void {
+    const index: number = findGameIndex(game.id);
+    if (index < 0) {
+        return;
+    }
+    games.splice(index, 1);
+}
--- src/backend/Scene-repository.ts
+++ src/backend/Scene-repository.ts
@@ -2,7 +2,7 @@
 export interface IScene {
     id: number,
     nextId : number,
-    ChoiceId: number,
+    choiceId: number,
     beforeId : number,
     talkingChar: string,
     text: string,
@@ -13,4 +13,46 @@ export interface IScenePictures{
     /*leftChar: string,
     rightChar: string,
     background: string*/
+}
+
+let nextId = 0;
+let scenes: IScene[] = [];
+
+function findSceneIndex(id: number) {
+    for (let i = 0; i < scenes.length; i++) {
+        if (scenes[i].id === id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+export function getAllScenes() {
+    return scenes;
+}
+
+export function getSceneById(id: number): IScene|undefined {
+    const index: number = findSceneIndex(id);
+    if (index < 0) {
+        return undefined;
+    }
+    return scenes[index];
+}
+
+export function addScene(nextId: number,choiceId: number,beforeId: number,talkingChar:string,text:string,pics:IScenePictures): IScene {
+    const scene: IScene = { id: nextId++, nextId: nextId, choiceId: choiceId,beforeId: beforeId,talkingChar: talkingChar,text:text,scenePics: pics};
+    scenes.push(scene);
+    return scene;
+}
+
+export function removeAllScenes(): void {
+    scenes.splice(0);
+}
+
+export function removeScene(scene: IScene): void {
+    const index: number = findSceneIndex(scene.id);
+    if (index < 0) {
+        return;
+    }
+    scenes.splice(index, 1);
 }
