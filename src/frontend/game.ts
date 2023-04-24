f7701a0c09def428164121a4a5078691180f8a65
Kaede25 <emma.walchshofer@gmail.com>
2023-04-24 17:07:58 +0200

    started with game.ts

---
+++ src/frontend/game.ts
@@ -0,0 +1,13 @@
+class Game{
+    id: number;
+    name: string;
+    pronouns: string[];
+    //scenes: Scene; TODO! merge with Mia
+    //infos: GameInfo; TODO! create Game info class
+    //loggedIn: boolean; TODO! check if needed when accounts exist
+
+    public playGame():boolean{
+        return false;
+    }
+
+}
