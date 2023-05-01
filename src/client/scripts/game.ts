ac73ce36413369e2b8db4fca8a7713d2b4f879b2
Kaede25 <emma.walchshofer@gmail.com>
2023-05-01 15:54:00 +0200

    added some implementation for

--- src/client/scripts/game.ts
+++ src/client/scripts/game.ts
@@ -1,9 +1,10 @@
 import {GameInfo} from "./game-info";
+import {Scene} from "./scene";
 export class Game{
     private readonly id: number; // must stay readonly, due to saving in database and identification
     private name: string; // game name might get changed later on, so it's not read only
     private pronouns: string[]; // TODO!! ask liss when this is gonna get set
-    //private scenes: Scene[]; TODO! merge with Mia
+    private scenes: Scene[];
     private infos: GameInfo; // description might get changed
     //private loggedIn: boolean; TODO! check if needed when accounts exist
 
@@ -18,7 +19,11 @@ export class Game{
      *  return: false = if game not finished true = if game is finished
      */
     public playGame():boolean{
-        return false;
+        let nextID:number = 0;
+        while (nextID != -1){ // TODO!! check if the last nextID is gonna be -1
+            //nextID = this.scenes[nextID].playScene(); TODO!! ask Mia to put the code for next scene into her scene class
+        }
+        return true;
     }
 
     /**
