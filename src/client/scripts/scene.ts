895c2e15bba56a3106b557d71bf28b41add435d8
Diabetisch <marceta.mia2006@gmail.com>
2023-05-03 12:45:23 +0200

    added constructor for scene and scene-pics

--- src/client/scripts/scene.ts
+++ src/client/scripts/scene.ts
@@ -1,15 +1,55 @@
 import {ScenePictures} from "./scene-pics";
 
 export class Scene{
-    id: number;
-    nextId: number;
-    nextId2: number;
-    prevId: number;
-    talkingCharacter: string;
-    text: string;
-    pictures: ScenePictures;
+
+    private id: number;
+    private nextId: number;
+    private nextId2: number; //if the scene has no decisions => nextId2 = null
+    private prevId: number;
+    private talkingCharacter: string;
+    private text: string;
+    private pictures: ScenePictures;
+
+    constructor(id: number, nextId: number, nextId2: number, prevId: number, talkingCharacter: string, text: string, pictures: ScenePictures) {
+        this.id = id;
+        this.nextId = nextId;
+        this.nextId2 = nextId2;
+        this.prevId = prevId;
+        this.talkingCharacter = talkingCharacter;
+        this. text = text;
+        this.pictures = pictures;
+    }
+    public playScene(): number{
+        return;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getNextId(): number {
+        return this.nextId;
+    }
+
+    public getNextId2(): number {
+        return this.nextId2;
+    }
+
+    public getPrevId(): number {
+        return this.prevId;
+    }
+
+    public getTalkingCharacter(): string {
+        return this.talkingCharacter;
+    }
+
+    public getText(): string {
+        return this.text;
+    }
+
+    public getPictures(): ScenePictures {
+        return this.pictures;
+    }
+
 }
 
-export function playScene(): number{
-    return;
-}
